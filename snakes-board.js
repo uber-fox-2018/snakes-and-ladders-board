a13c28c58135a0c29d3b088de4b7fa26d5405871
Harles Bayu Anggara <bayuanggaraharles@gmail.com>
2018-07-11 16:33:44 +0700

    Update
    mengganti built-in reverse array dengan reverse manual

--- snakes-board.js
+++ snakes-board.js
@@ -10,7 +10,7 @@ function generateBoard(num){
                 numTimes--
             }
             if (i % 2 == 0) result.push(tmp)
-            else result.push(tmp.reverse())
+            else result.push(reverseArray(tmp))
         }
     } else {
         for(let i = 1; i <= num; i++){
@@ -20,10 +20,23 @@ function generateBoard(num){
                 numTimes--
             }
             if (i % 2 !== 0) result.push(tmp)
-            else result.push(tmp.reverse())
+            else result.push(reverseArray(tmp))
         }
     }
     return result
+}
+
+function reverseArray(input){
+  for(let i = 0; i < input.length; i++){
+    for(let j = 0; j < input.length; j++){
+      if(input[j] > input[j+1]){
+        let sorting = input[j]
+        input[j] = input[j+1]
+        input[j+1] = sorting
+      }
+    }
+  }
+  return input
 }
 
 console.log(generateBoard(15))

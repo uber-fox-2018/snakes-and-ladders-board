5cc52440f99aa94d61c814499e5afa0960d0f3d2
imamaguss <imam.agussopian@gmail.com>
2018-07-11 20:00:16 +0700

    add snack and ladders boards

---
+++ index.js
@@ -0,0 +1,30 @@
+function snakesAndLadders(rows, cols) {
+  const output = [];
+  let count = rows * cols;
+  for(let i = 0; i < rows; i++) {
+    let arrNum = [];
+    for(let j = 1; j <= cols; j++) {
+      arrNum.push(count);
+      count--;
+    }   
+    output.push(arrNum);
+  }
+
+  for(let i = 0; i < output.length; i++) {
+    if(i % 2 !== 0) {
+      for(let j = 0; j < output[i].length-1; j++) {
+        if(output[i][j] > output[i][j+1]) {
+          let tmp = output[i][j];
+          output[i][j] = output[i][j+1];
+          output[i][j+1] = tmp;
+          j = -1;
+        }
+      }
+    }        
+  }
+
+  return output;
+
+}
+
+console.log(snakesAndLadders(10,10));
